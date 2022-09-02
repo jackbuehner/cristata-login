492a4272f02d03fbbb2ce56166edc406ff6bf6c0
Jack Buehner <git@jackbuehner.com>
2022-09-02 01:19:43 -0400

    feat: add button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let elem: HTMLButtonElement | undefined = undefined;
+</script>
+
+<button bind:this={elem} on:click on:dblclick><slot /></button>
+
+<style>
+	button {
+		height: 32px;
+		font-family: 'Rubik', sans-serif;
+		font-weight: 600;
+		font-size: 16px;
+		background-color: transparent;
+		border: 2px solid var(--textColorLighter);
+		color: var(--textColorLighter);
+		border-radius: 3px;
+		min-width: 60px;
+		outline: none;
+		transition: background-color 200ms;
+	}
+	button:hover,
+	button:focus {
+		background-color: rgba(255, 255, 255, 0.06);
+	}
+	button:active {
+		background-color: rgba(255, 255, 255, 0.12);
+	}
+</style>
