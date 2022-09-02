60584e86becd45b61418e8b1b761479e67e7ea10
Jack Buehner <git@jackbuehner.com>
2022-09-02 01:55:10 -0400

    feat: add sign out page

---
+++ src/routes/[tenant]/sign-out/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import Header from '$components/Header.svelte';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		fetch(`http://127.0.0.1:3000/auth/clear`, {
+			method: 'post',
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			cache: 'no-cache'
+		}).finally(() => {
+			goto(`/${$page.params.tenant}`);
+		});
+	});
+</script>
+
+<Header heading="Signing out of Cristata" caption="Please wait a moment" />
