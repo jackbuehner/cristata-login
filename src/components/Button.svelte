0bd3524a860e9e6cca154072b2aba7bad0884b11
Jack Buehner <git@jackbuehner.com>
2022-09-03 20:25:54 -0400

    feat: support button as link

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
 	export let elem: HTMLButtonElement | undefined = undefined;
 	export let style: string = '';
+	export let href: string = '';
 </script>
 
-<button bind:this={elem} on:click on:dblclick {style}><slot /></button>
+{#if href}
+	<a {href} {style}><slot /></a>
+{:else}
+	<button bind:this={elem} on:click on:dblclick {style}><slot /></button>
+{/if}
 
 <style>
-	button {
+	button,
+	a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		text-decoration: none;
+		cursor: default;
 		height: 32px;
 		font-family: 'Rubik', sans-serif;
 		font-weight: 600;
@@ -20,10 +31,13 @@
 		transition: background-color 200ms;
 	}
 	button:hover,
-	button:focus {
+	a:hover,
+	button:focus,
+	a:focus {
 		background-color: rgba(255, 255, 255, 0.06);
 	}
-	button:active {
+	button:active,
+	a:active {
 		background-color: rgba(255, 255, 255, 0.12);
 	}
 </style>
