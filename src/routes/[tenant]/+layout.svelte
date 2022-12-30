0fb24d9b2f5568d2b799eee440b2a03e1a2b2748
Jack Buehner <git@jackbuehner.com>
2022-12-30 01:44:31 -0500

    feat: automatically continue sign in per url search param

--- src/routes/[tenant]/+layout.svelte
+++ src/routes/[tenant]/+layout.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { PUBLIC_APP_URL } from '$env/static/public';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
+	$: shouldContinue =
+		$page.url.searchParams.get('continue') === '1' && data.currentUser && data.tenant;
+
+	const returnToUrl = () => {
+		const searchParams = $page.url.searchParams;
+		const returnUrl = searchParams.get('return') || `${PUBLIC_APP_URL}/${data.tenant.name}`;
+		goto(returnUrl);
+	};
+
+	$: if (shouldContinue) returnToUrl();
 </script>
 
 {#if data.currentUser === undefined}
 	<h1>Checking...</h1>
+{:else if shouldContinue}
+	<h1>Signing in...</h1>
 {:else}
 	<slot />
 {/if}
