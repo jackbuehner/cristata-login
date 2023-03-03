9f51af663f87ab199b4dcdecfd0ceac0849724cd
Jack Buehner <git@jackbuehner.com>
2023-03-03 03:50:40 -0500

    fix: set correct cookie domain

--- src/utils/modifySessionCookie.ts
+++ src/utils/modifySessionCookie.ts
@@ -3,7 +3,7 @@ import { browser } from '$app/environment';
 export function modifySessionCookie() {
 	if (browser) {
 		// Get all cookies accessible via javascipt.
-		// The server should only disable `httpOnly` on the session cookie
+		// The server should only disable `httpOnly` on the session cookie; domain=
 		// when the return URL is to `https://tauri.local`.
 		const cookies = document.cookie.split('; ').map((cookie) => cookie.split(/=(.*)/s, 2));
 
@@ -18,8 +18,13 @@ export function modifySessionCookie() {
 		// If the session cookies are available, update them to allow cross-site access
 		// so they can be sent from `https://tauti.local`.
 		if (session && sig) {
-			document.cookie = `${session[0]}=${session[1]}; SameSite=None; Secure; Expires=Tue, 19 Jan 2038 04:14:07 GMT`;
-			document.cookie = `${sig[0]}=${sig[1]}; SameSite=None; Secure; Expires=Tue, 19 Jan 2038 04:14:07 GMT`;
+			if (session[0] === '__Secure-cristata-session') {
+				document.cookie = `${session[0]}=${session[1]}; SameSite=None; Secure; Expires=Tue, 19 Jan 2038 04:14:07 GMT; domain=.cristata.app`;
+				document.cookie = `${sig[0]}=${sig[1]}; SameSite=None; Secure; Expires=Tue, 19 Jan 2038 04:14:07 GMT; domain=.cristata.app`;
+			} else {
+				document.cookie = `${session[0]}=${session[1]}; SameSite=None; Secure; Expires=Tue, 19 Jan 2038 04:14:07 GMT`;
+				document.cookie = `${sig[0]}=${sig[1]}; SameSite=None; Secure; Expires=Tue, 19 Jan 2038 04:14:07 GMT`;
+			}
 		}
 	}
 }
